cc77bb36e634354640f2d3a30a9a5adbf48eb055
shalishi4062 <shali.shi@insa-lyon.fr>
2017-01-17 17:31:22 +0100

    Update analog.cpp

--- TP4/bin/analog.cpp
+++ TP4/bin/analog.cpp
@@ -1,126 +1,141 @@
 // tp2.cpp : Defines the entry point for the console application.
 //
 
+
 #include <string>
-#include <cstring>
 #include "CollectionDeLog.h"
 #include <iostream>
-
-
 using namespace std;
 
-int  main(void) {
+int main(string commandes)
+{
+	
+	//const char * commande="$./analog [-e -t 45]nom.log";
+	//string commandes("[-e -t 45]nom.log");
+	//cout<<commandes<<endl;
+	//cout<<"here"<<endl;
+	
+	int begin=0;
+			begin=commandes.find(" ",begin);
+			while(begin!=-1)
+			{
+			commandes.replace(begin,1,"");
+			begin=commandes.find(" ",begin);
+			}
+			//cout<<"commandes"<<commandes<<endl;
+		if(commandes.at(0)=='[')
+		{
+			string nomdeficher=commandes.substr(commandes.find("]")+1);
+			//cout<<"nomdeficher"<<nomdeficher<<endl; 
+			//system("pause");
+			int num=commandes.find("]")-commandes.find("[");
+			string option=commandes.substr(10,num-1);
+			//cout<<"option"<<option<<endl;
+			//system("pause");
+			/*string suboption1=option.substr(0,2);
+            cout<<"suboption1"<<suboption1<<endl;
+			system("pause");
+			*/
+			if(option.find("-g")!=-1)
+			{
+				int position =option.find(".dot");
+				string nomdegraphe=option.substr(option.find("-g"),position+4); 
+				//cout<<"nomdegraphe"<<nomdegraphe<<endl;
+			}
 
-    char * commande;
-    cin >> commande;
-    string commandetypes(commande);
-    //cout<<commandetypes<<endl;
-    //cout<<"here"<<endl;
-    const char *condi = "$./analog ";
-    int begin = 0;
-    begin = commandetypes.find(" ", begin);
-    while (begin != -1) {
-        commandetypes.replace(begin, 1, "");
-        begin = commandetypes.find(" ", begin);
-    }
-    cout << "commandetypes" << commandetypes << endl;
+			if(option.find("-t")!=-1)
+			{
+				//system("pause");
+			string heure11=option.substr(option.find("-t")+1,2);
+				cout<<heure11<<endl;
+				char& heure1 = heure11.at(0);
+				char& heure2 = heure11.at(1);
+				int time1=heure1-'0';
+				if(time1>2||time1<0)
 
-    if (commandetypes.at(9) == '[') {
-        string nomdeficher = commandetypes.substr(commandetypes.find("]") + 1);
-        cout << "nomdeficher" << nomdeficher << endl;
-       // system("pause");
-        int num = commandetypes.find("]") - commandetypes.find("[");
-        string option = commandetypes.substr(10, num - 1);
-        cout << "option" << option << endl;
-      //  system("pause");
-        /*string suboption1=option.substr(0,2);
-        cout<<"suboption1"<<suboption1<<endl;
-        system("pause");
-        */
-        if (option.find("-g") != -1) {
-            int position = option.find(".dot");
-            string nomdegraphe = option.substr(option.find("-g"), position + 4);
-            cout << "nomdegraphe" << nomdegraphe << endl;
-        }
+				{
+					std::cout<<"invalide time"<<endl;
+					//system ("pause");
+					return 0;
+				}
+				int time2=heure2-'0';
+				if(time1>9||time1<0)
+				{
+					std::cout<<"invalide time"<<endl;
+					//system ("pause");
+					return 0;
+				}
+				int heure=time1*10+time2;
+			}
+			if(option.find("-g")!=-1&&option.find("-t")==-1&&option.find("-e")==-1)
+				{
+				    CollectionDeLog(nomdeficher,'n',-1, 'g', nomdegraphe);
+					//system ("pause");
+					return 0;
+				}
+			    if(option.find("-g")!=-1&&option.find("-t")!=-1&&option.find("-e")!=-1)
+				{
+					CollectionDeLog(nomdeficher, 'e', heure,'g', nomdegraphe);
+					//system ("pause");
+					return 0;
+				}
+				 if(option.find("-g")!=-1&&!option.find("-t")==-1&&option.find("-e")!=-1)
+				 {
 
-        if (option.find("-t") != -1) {
-     //       system("pause");
-            string heure11 = option.substr(option.find("-t") + 1, 2);
-            cout << heure11 << endl;
-            char &heure1 = heure11.at(0);
-            char &heure2 = heure11.at(1);
-            int time1 = heure1 - '0';
-            if (time1 > 2 || time1 < 0) {
-                std::cout << "invalide time" << endl;
-      //          system("pause");
-                return 0;
-            }
-            int time2 = heure2 - '0';
-            if (time1 > 9 || time1 < 0) {
-                std::cout << "invalide time" << endl;
-        //        system("pause");
-                return 0;
-            }
-            int heure = time1 * 10 + time2;
-        }
-        if (option.find("-g") != -1 && option.find("-t") == -1 && option.find("-e") == -1) {
-        //    CollectionDeLog(nomdeficher, 'g', nomdegraphe);
-            //system ("pause");
-            return 0;
-        }
-        if (option.find("-g") != -1 && option.find("-t") != -1 && option.find("-e") != -1) {
-      //      CollectionDeLog(nomdeficher, 'e', heure,'g', nomdegraphe);
-            //system ("pause");
-            return 0;
-        }
-        if (option.find("-g") != -1 && !option.find("-t") == -1 && option.find("-e") != -1) {
-
-         //   CollectionDeLog(nomdeficher,heure, 'g', nomdegraphe);
-            //system ("pause");
-            return 0;
-        }
-        if (option.find("-g") != -1 && option.find("-t") != -1 && option.find("-e") == -1) {
-            //CollectionDeLog(nomdeficher, 'e', 'g', nomdegraphe);
-            //system ("pause");
-            //return 0;
-        }
-        if (option.find("-g") == -1 && option.find("-t") != -1 && option.find("-e") != -1) {
-            //CollectionDeLog(nomdeficher, 'e',heure);
-            //system ("pause");
-            //return 0;
-        }
-        if (option.find("-g") == -1 && option.find("-t") == -1 && option.find("-e") != -1) {
-            //CollectionDeLog(nomdeficher, 'e');
-            //system ("pause");
-            //return 0;
-        }
-        if (option.find("-g") == -1 && option.find("-t") != -1 && option.find("-e") == -1) {
-            //CollectionDeLog(nomdeficher,heure);
-            //system ("pause");
-            //return 0;
-        }
-        if (option.find("-g") == -1 && option.find("-t") == -1 && option.find("-e") == -1) {
-            //CollectionDeLog(nomdeficher);
-            //system ("pause");
-            //return 0;
-        }
+				    CollectionDeLog(nomdeficher,'e',-1, 'g', nomdegraphe);
+					//system ("pause");
+					return 0;
+				 }
+				  if(option.find("-g")!=-1&&option.find("-t")!=-1&&option.find("-e")==-1)
+				 {
+				    CollectionDeLog(nomdeficher, 'n',heure, 'g', nomdegraphe);
+					//system ("pause");
+					return 0;
+				 }
+				  if(option.find("-g")==-1&&option.find("-t")!=-1&&option.find("-e")!=-1)
+				 {
+				    CollectionDeLog(nomdeficher, 'e',heure);
+					system ("pause");
+					return 0;
+				 }
+				  if(option.find("-g")==-1&&option.find("-t")==-1&&option.find("-e")!=-1)
+				 {
+				    CollectionDeLog(nomdeficher, 'e');
+					//system ("pause");
+					return 0;
+				 }
+				  if(option.find("-g")==-1&&option.find("-t")!=-1&&option.find("-e")==-1)
+				 {
+				    CollectionDeLog(nomdeficher,'n',heure);
+					//system ("pause");
+					return 0;
+				 }
+				    if(option.find("-g")==-1&&option.find("-t")==-1&&option.find("-e")==-1)
+				 {
+				    CollectionDeLog(nomdeficher);
+					//system ("pause");
+					return 0;
+				 }
 
 
-    } else {
-        string nomdeficher = strstr(commande, " ") + 1;
-        cout << "nomdeficher" << nomdeficher << endl;
-        //CollectionDeLog(nomdeficher, 'n');
+		}
+			
+		else
+		{
+			if(commandes.find(".log")!=-1)
+			{
+			string nomdeficher=commandes.substr(9);
+			cout<<"nomdeficher"<<nomdeficher<<endl;
+			//CollectionDeLog(nomdeficher, 'n');
+			}
+			else
+			{
+				cout<<"your docutement must end with .log"<<endl;
+			}
+		}
 
-    }
-
-
-    /*else{
-
-                cout<<"wrong commande"<<endl;
-        system ("pause");
-        return 0;
-    }
-    */
-   // system("pause");
-    return 0;
+	//system ("pause");
+	return 0;
 }
+
+	
