5bcb58ebe34ea6664516ed3147e972f7d002e68e
shikshak <ft.mezidi@gmail.com>
2017-01-17 17:26:45 +0100

    Update CollectionDeLog.cpp

--- TP4/bin/CollectionDeLog.cpp
+++ TP4/bin/CollectionDeLog.cpp
@@ -51,7 +51,7 @@ CollectionDeLog::CollectionDeLog(string nf, char e, int h, char topdix, string n
     cout << "Appel au constructeur de <CollectionDeLog>" << endl;
 #endif
 
-    if (e = 'e'){
+    if (e == 'e'){
         exclusion = true;
     }else{
         exclusion = false;
@@ -60,13 +60,13 @@ CollectionDeLog::CollectionDeLog(string nf, char e, int h, char topdix, string n
     if (h != -1){
         heure = h;
     }else{
-        heure = -1;
+        heure == -1;
     }
 
-    if (topdix ='o'){
+    if (topdix == 'o'){
         RemplirMapTopDix(nf);
         AfficherTopDix();
-    }else if (topdix = 'g'){
+    }else if (topdix == 'g'){
         RemplirMapGraph(nf);
         EnregistrerGraph(nfGraph);
     }
@@ -90,8 +90,8 @@ void CollectionDeLog::RemplirMapTopDix(string nf) {
     ifstream file ( nf.c_str() );
         if(file.good()){
             while(file >> l){
-                //verification du triE
-                if( (exclusion && l.GetType() !="js") && l.GetHeure() == heure ) { // j'ai pas mis les extensions
+                //verification
+                if( (exclusion && l.GetType() !="js") && ( heure != -1 && l.GetHeure() == heure) ) { // j'ai pas mis les extensions -> faire collection
                     //verification de l'existence de la cible
                     if (myMapTopDix.find(l.cible) != myMapTopDix.end()) {
                         myMapTopDix[l.cible] += 1;
@@ -113,7 +113,6 @@ void CollectionDeLog::RemplirMapGraph(string nf) {
 
 }
 
-
 void CollectionDeLog::AfficherTopDix() const {
 #ifdef MAP
     cout << "Appel de AfficherTopDix de <CollectionDeLog>" << endl;
@@ -121,26 +120,31 @@ void CollectionDeLog::AfficherTopDix() const {
 
     Log l;
 
-    vector<int, string> vectorTopDix();
-    mapGraph::const_iterator it;
+    multiset <int, string> setTopDix;
+    setTopDix mySetTopDix;
 
-    for(it = myMapGraph.begin(); it != myMapGraph.end(); ++it){
-        //vector.insert();
+    mapGraph::const_iterator itmap;
+
+    for(itmap = myMapGraph.begin(); itmap != myMapGraph.end(); ++itmap){
+        mySetTopDix.insert(itmap->second, itmap->first);
     }
 
-    //sort(vectorTopDix.begin(), vectorTopDix.end());
+    setTopDix::key_compare mycomp = mymultimap.key_comp();
 
-    int compteur =0;
+    int compt =0;
+    setTopDix::const_iterator itset;
 
     cout << "Le top 10 est :" << endl;
-/*
-    for(it = myMapTopDix.begin() ; compteur >10 ; ++it){
-        cout << "page : " << it->first << " " << it->second << " hits." << endl; // a changer avec la syntaxe du tp
+
+    for(itset = mySetTopDix.begin(); compt >10 && itset !=mySetTopDix.end(); ++itset){
+
+        cout << "page : " << itset->second << " " << itset->first << " hits." << endl; // a changer avec la syntaxe du tp
         compteur++;
+
     }
 
     cout << "fin du top 10 !" << endl;
-*/
+
 }
 
 void CollectionDeLog::EnregistrerGraph(string nfGraph) const {
