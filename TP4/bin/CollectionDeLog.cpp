bdf617c1c79e8feeda334e28f8bcf420f7f7714b
shikshak <ft.mezidi@gmail.com>
2017-01-17 18:07:12 +0100

    Update CollectionDeLog.cpp

--- TP4/bin/CollectionDeLog.cpp
+++ TP4/bin/CollectionDeLog.cpp
@@ -1,155 +1,109 @@
 /*************************************************************************
-                           CollectionDeLog  -  description
+                           Log  -  description
                              -------------------
-    début                : 10 janv. 2017
-    copyright            : (C) 2017 par jcharlesni
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
 *************************************************************************/
 
-//---------- Réalisation du module <CollectionDeLog> (fichier CollectionDeLog.cpp) -----
+//---------- Réalisation de la classe <Log> (Log Log.cpp) ------------
 
-/////////////////////////////////////////////////////////////////  INCLUDE
+//---------------------------------------------------------------- INCLUDE
+
 //-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
 
 //------------------------------------------------------ Include personnel
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include "CollectionDeLog.h"
-using namespace std;
+#include "Log.h"
 
-///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
+const char SEP = ' ';
+//---------------------------------------------------- Variables de classe
 
-//------------------------------------------------------------------ Types
+//----------------------------------------------------------- Types privés
 
-//---------------------------------------------------- Variables statiques
 
-//------------------------------------------------------ Fonctions privées
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
 
-//////////////////////////////////////////////////////////////////  PUBLIC
-//---------------------------------------------------- Fonctions publiques
+//----------------------------------------------------- Méthodes publiques
+int Log::GetHeure()
+// Algorithme :
+//
+{   string date = Infos.date;
+    size_t pos = date.find_first_of(':');
+    string res = "";
+    res = date.substr(pos+1, 2);
+    int heure = stoi(res);
+    return heure;
+} //----- Fin de GetHeure
 
-class const_iterator;
+string Log::GetType()
+// Algortihme :
+//
+{   string c = cible;
+    size_t pos = c.find_first_of('.');
+    string res;
+    res = c.substr(pos+1);
+    return res;
+} //----- Fin de GetType
 
-class const_iterator;
+//------------------------------------------------- Surcharge d'opérateurs
 
-CollectionDeLog::CollectionDeLog ( ){
+//-------------------------------------------- Constructeurs - destructeur
+Log::Log ( const Log & unLog )
+// Algorithme :
+//
+{
 #ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
+    cout << "Appel au constructeur de copie de <Log>" << endl;
 #endif
+} //----- Fin de Log (constructeur de copie)
+
+
+/*Log::Log(string c, Informations i) ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Log>" << endl;
+#endif
+} //----- Fin de Log*/
+
+
+Log::~Log ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Log>" << endl;
+#endif
+} //----- Fin de ~Log
+
+
+//------------------------------------------------------------------ PRIVE
+istream & operator >> (istream & is, Log & f)
+{   getline(is, f.Infos.ip, SEP);
+    getline(is, f.Infos.logname, SEP);
+    getline(is, f.Infos.name, SEP);
+    getline(is, f.Infos.date, SEP);
+    getline(is, f.Infos.requete, SEP);
+    getline(is, f.cible, SEP);
+    getline(is, f.Infos.type, SEP);
+    getline(is, f.Infos.status, SEP);
+    getline(is, f.Infos.data, SEP);
+    getline(is, f.Infos.referent, SEP);
+    getline(is, f.Infos.navigateur);
+    return is;
 }
 
-CollectionDeLog::CollectionDeLog (const CollectionDeLog & unCollectionDeLog){
-#ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
-#endif
+ostream & operator << (ostream & os, Log & f)
+{   os << f.Infos.ip << SEP << f.Infos.logname << SEP << f.Infos.name << SEP << f.Infos.date << SEP << f.Infos.requete << SEP << f.cible << SEP << f.Infos.status;
+    os << SEP << f.Infos.data << SEP << f.Infos.referent << SEP << f.Infos.navigateur << endl;
+    return os;
 }
+//----------------------------------------------------- Méthodes protégées
 
+//------------------------------------------------------- Méthodes privées
 
-CollectionDeLog::CollectionDeLog(string nf, char e, int h, char topdix, string nfGraph) {
-#ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
-#endif
-
-    if (e == 'e'){
-        exclusion = true;
-    }else{
-        exclusion = false;
-    }
-
-    if (h != -1){
-        heure = h;
-    }else{
-        heure == -1;
-    }
-
-    if (topdix == 'o'){
-        RemplirMapTopDix(nf);
-        AfficherTopDix();
-    }else if (topdix == 'g'){
-        RemplirMapGraph(nf);
-        EnregistrerGraph(nfGraph);
-    }
-
-}
-
-
-CollectionDeLog::~CollectionDeLog() {
-#ifdef MAP
-    cout << "Appel au destructeur de <CollectionDeLog>" << endl;
-#endif
-}
-
-void CollectionDeLog::RemplirMapTopDix(string nf) {
-#ifdef MAP
-    cout << "Appel de AjouterTopDix de <CollectionDeLog>" << endl;
-#endif
-
-    Log l;
-
-    ifstream file ( nf.c_str() );
-        if(file.good()){
-            while(file >> l){
-                //verification
-                if( (exclusion && exclus.find(l.GetType())!=exclus.end()) && ( heure != -1 && l.GetHeure() == heure) ) { // j'ai pas mis les extensions -> faire collection
-                    //verification de l'existence de la cible
-                    if (myMapTopDix.find(l.cible) != myMapTopDix.end()) {
-                        myMapTopDix[l.cible] += 1;
-                    } else {
-                        if(!l.cible.empty()){
-                            myMapTopDix.insert(make_pair(l.cible, 1));
-                        }
-                    }
-                }
-            }
-        }
-
-}
-
-void CollectionDeLog::RemplirMapGraph(string nf) {
-#ifdef MAP
-    cout << "Appel de AjouterGraph de <CollectionDeLog>" << endl;
-#endif
-
-}
-
-void CollectionDeLog::AfficherTopDix() const {
-#ifdef MAP
-    cout << "Appel de AfficherTopDix de <CollectionDeLog>" << endl;
-#endif
-
-    Log l;
-
-    multiset <int, string> setTopDix;
-    setTopDix mySetTopDix;
-
-    mapGraph::const_iterator itmap;
-
-    for(itmap = myMapGraph.begin(); itmap != myMapGraph.end(); ++itmap){
-        mySetTopDix.insert(itmap->second, itmap->first);
-    }
-
-    setTopDix::key_compare mycomp = mymultimap.key_comp();
-
-    int compt =0;
-    setTopDix::const_iterator itset;
-
-    cout << "Le top 10 est :" << endl;
-
-    for(itset = mySetTopDix.begin(); compt >10 && itset !=mySetTopDix.end(); ++itset){
-
-        cout << "page : " << itset->second << " " << itset->first << " hits." << endl; // a changer avec la syntaxe du tp
-        compteur++;
-
-    }
-
-    cout << "fin du top 10 !" << endl;
-
-}
-
-void CollectionDeLog::EnregistrerGraph(string nfGraph) const {
-#ifdef MAP
-    cout << "Appel de EnregistrerGraph de <CollectionDeLog>" << endl;
-#endif
-
-}
