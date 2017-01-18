8016263db88093297bc1466d6865bedcf2e5c9dd
shikshak <ft.mezidi@gmail.com>
2017-01-18 01:31:48 +0100

    Update CollectionDeLog.cpp

--- TP4/bin/CollectionDeLog.cpp
+++ TP4/bin/CollectionDeLog.cpp
@@ -3,7 +3,7 @@
                              -------------------
     début                : 10 janv. 2017
     copyright            : (C) 2017 par jcharlesni
-*************************************************************************/
+ *************************************************************************/
 
 //---------- Réalisation du module <CollectionDeLog> (fichier CollectionDeLog.cpp) -----
 
@@ -26,128 +26,216 @@
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
 
-class const_iterator;
-
-class const_iterator;
 
 CollectionDeLog::CollectionDeLog ( ){
 #ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
+	cout << "Appel au constructeur de <CollectionDeLog>" << endl;
 #endif
 }
 
 CollectionDeLog::CollectionDeLog (const CollectionDeLog & unCollectionDeLog){
 #ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
+	cout << "Appel au constructeur de <CollectionDeLog>" << endl;
 #endif
 }
 
 
 CollectionDeLog::CollectionDeLog(string nf, char e, int h, char topdix, string nfGraph) {
 #ifdef MAP
-    cout << "Appel au constructeur de <CollectionDeLog>" << endl;
+	cout << "Appel au constructeur de <CollectionDeLog>" << endl;
 #endif
 
-    nomFichier = nf;
-    if (e == 'e'){
-        exclusion = true;
-    }else{
-        exclusion = false;
-    }
-    heure = h;
+	nomFichier = nf;
+	if (e == 'e'){
+		exclusion = true;
+	}else{
+		exclusion = false;
+	}
+	heure = h;
 
-    if (topdix == 'o'){
-        AfficherTopDix();
-    }else if (topdix == 'g'){
-        EnregistrerGraph(nfGraph);
-    }
+	if (topdix == 'o'){
+		AfficherTopDix();
+	}else if (topdix == 'g'){
+		EnregistrerGraph(nfGraph);
+	}
 
 }
 
 
 CollectionDeLog::~CollectionDeLog() {
 #ifdef MAP
-    cout << "Appel au destructeur de <CollectionDeLog>" << endl;
+	cout << "Appel au destructeur de <CollectionDeLog>" << endl;
 #endif
 }
 
 void CollectionDeLog::RemplirMapTopDix(){
 #ifdef MAP
-    cout << "Appel de AjouterTopDix de <CollectionDeLog>" << endl;
+	cout << "Appel de AjouterTopDix de <CollectionDeLog>" << endl;
 #endif
-    if(heure!=-1) cout << "Warning : only hits between " << heure << "h and "<< heure+1 << "h have been taken into account" << endl;
-    Log l;
+	if(heure!=-1) cout << "Warning : only hits between " << heure << "h and "<< heure+1 << "h have been taken into account" << endl;
+	Log l;
 
-    ifstream file ( nomFichier.c_str() );
-        if(file.good()){
-            while(file >> l){
-                //verification
-                int s = 0;
-                if(isdigit(l.Infos.status[0])) s= stoi(l.Infos.status);
-                if(s==200) {
-                    if (((exclusion && exclus.find(l.GetType()) == exclus.end()) &&
-                         (heure != -1 && l.GetHeure() == heure)) || (!exclusion && heure == -1) ||
-                        (!exclusion && heure != -1 && l.GetHeure() == heure) || (heure == -1 && (exclusion &&
-                                                                                                 exclus.find(
-                                                                                                         l.GetType()) ==
-                                                                                                 exclus.end()))) { // j'ai pas mis les extensions -> faire collection
-                        //verification de l'existence de la cible
-                        if (myMapTopDix.size() != 0 && myMapTopDix.find(l.cible) != myMapTopDix.end()) {
-                            myMapTopDix[l.cible] += 1;
-                        } else {
-                            if (!l.cible.empty()) {
-                                myMapTopDix.insert(make_pair(l.cible, 1));
-                            }
-                        }
-                    }
-                }
-            }
-        }
+	ifstream file ( nomFichier.c_str() );
+	if(file.good()){
+		while(file >> l){
+			//verification
+			int s = 0;
+			if(isdigit(l.Infos.status[0])) s= stoi(l.Infos.status);
+			if(s==200) {
+				if (((exclusion && exclus.find(l.GetType()) == exclus.end()) &&
+						(heure != -1 && l.GetHeure() == heure)) || (!exclusion && heure == -1) ||
+						(!exclusion && heure != -1 && l.GetHeure() == heure) || (heure == -1 && (exclusion &&
+								exclus.find(
+										l.GetType()) ==
+												exclus.end()))) { // j'ai pas mis les extensions -> faire collection
+					//verification de l'existence de la cible
+					if (myMapTopDix.size() != 0 && myMapTopDix.find(l.cible) != myMapTopDix.end()) {
+						myMapTopDix[l.cible] += 1;
+					} else {
+						if (!l.cible.empty()) {
+							myMapTopDix.insert(make_pair(l.cible, 1));
+						}
+					}
+				}
+			}
+		}
+	}
 
 }
 
-void CollectionDeLog::RemplirMapGraph(string nf) {
+void CollectionDeLog::RemplirMapGraph() {
 #ifdef MAP
-    cout << "Appel de AjouterGraph de <CollectionDeLog>" << endl;
+	cout << "Appel de RemplirMapGraph de <CollectionDeLog>" << endl;
 #endif
 
+	Log l;
+	string cibref;
+	dessin dessi;
+
+	ifstream file ( nomFichier.c_str() );
+	if(file.good()){
+		while(file >> l){
+			//verification
+			int s = 0;
+			if(isdigit(l.Infos.status[0])) s= stoi(l.Infos.status);
+			if(s==200) {
+				if (((exclusion && exclus.find(l.GetType()) == exclus.end()) &&
+						(heure != -1 && l.GetHeure() == heure)) || (!exclusion && heure == -1) ||
+						(!exclusion && heure != -1 && l.GetHeure() == heure) || (heure == -1 && (exclusion &&
+								exclus.find(
+										l.GetType()) ==
+												exclus.end()))) {
+					cibref = l.cible + l.Infos.referent; //clé de la map
+
+					//verification de l'existence de la cible
+					if (myMapGraph.size() != 0 && myMapGraph.find(cibref) != myMapGraph.end()) {
+						myMapGraph[cibref].compteur += 1;
+					} else {
+
+						if (!l.cible.empty()) {
+							dessi.compteur = 1;
+							dessi.cible = l.cible;
+							dessi.referent = l.Infos.referent; //contient le nombre de fois que referent -> cible a été effectué
+							myMapGraph.insert(make_pair(cibref, dessi));
+						}
+
+					}
+				}
+			}
+		}
+	}
 }
 
 void CollectionDeLog::AfficherTopDix(){
 #ifdef MAP
-    cout << "Appel de AfficherTopDix de <CollectionDeLog>" << endl;
+	cout << "Appel de AfficherTopDix de <CollectionDeLog>" << endl;
 #endif
-    RemplirMapTopDix();
-    Log l;
 
-    typedef multimap <int, string> setTopDix;
-    setTopDix mySetTopDix;
+	RemplirMapTopDix();
 
-    mapTopDix::const_iterator itmap;
-    for(itmap = myMapTopDix.begin(); itmap != myMapTopDix.end(); ++itmap){
-        mySetTopDix.insert(make_pair(itmap->second, itmap->first));
-    }
+	Log l;
 
-    int compt =0;
-    setTopDix::const_iterator itset;
+	typedef multimap <int, string> setTopDix;
+	setTopDix mySetTopDix;
 
-    for(itset = mySetTopDix.end() ; compt <11 && itset !=mySetTopDix.begin(); --itset){
-        if(itset!=mySetTopDix.end()) {
-            cout << itset->second << " (" << itset->first << " hits)" << endl; // a changer avec la syntaxe du tp
-        }
-        compt++;
+	mapTopDix::const_iterator itmap;
 
-    }
-    if(compt<11) cout << itset->second << "  (" << itset->first << " hits)" << endl; // a changer avec la syntaxe du tp
+	for(itmap = myMapTopDix.begin(); itmap != myMapTopDix.end(); ++itmap){
+		mySetTopDix.insert(make_pair(itmap->second, itmap->first));
+	}
+
+	int compt =0;
+	setTopDix::const_iterator itset;
+
+	for(itset = mySetTopDix.end() ; compt <11 && itset !=mySetTopDix.begin(); --itset){
+		if(itset!=mySetTopDix.end()) {
+			cout << itset->second << " (" << itset->first << " hits)" << endl;
+		}
+		compt++;
+
+	}
+	if(compt<11) cout << itset->second << "  (" << itset->first << " hits)" << endl; // ca devrait pas etre 10
 
 }
 
 void CollectionDeLog::EnregistrerGraph(string nfGraph){
 #ifdef MAP
-    cout << "Appel de EnregistrerGraph de <CollectionDeLog>" << endl;
+	cout << "Appel de EnregistrerGraph de <CollectionDeLog>" << endl;
 #endif
 
-    cout << "Dot-file " << nfGraph << " generated" << endl;
-    AfficherTopDix();
-    RemplirMapGraph(nfGraph);
+	cout << "Dot-file " << nfGraph << " generated" << endl;
+	AfficherTopDix();
+	RemplirMapGraph();
+
+	typedef map <string, int> mapNode; //elle contiendra le nom d'une page (cible ou referent) et le numero de node associe
+	mapNode myMapNode;
+
+	int nombreNode = 0;
+
+	mapTopDix::const_iterator itmap;
+
+	dessin dessi;
+
+	ofstream ouf;
+	ouf.open(nfGraph, ios::trunc);
+	ouf.close();
+	ouf.open(nfGraph, ios::app);
+	if(ouf.good())
+	{
+		ouf << "digraph {" << endl;
+
+		//on associe toutes les cibles et tous les referents à un numero de node
+		//et on l'ecrit sur le fichier
+		for(auto itmap = myMapGraph.begin(); itmap != myMapGraph.end(); ++itmap){
+
+			dessi = itmap->second;
+			// on verifie si on a deja dessine un node de la cible
+			if (myMapNode.find(dessi.cible) == myMapNode.end()) {
+				myMapNode.insert(make_pair(dessi.cible, nombreNode));
+				ouf << "node" << nombreNode << " [label=\"" << dessi.cible << "\"];" << endl;
+				nombreNode+=1;
+			}
+			// on verifie si on a deja dessine un node du refenrennt
+			if (myMapNode.find(dessi.referent) == myMapNode.end()) {
+				myMapNode.insert(make_pair(dessi.referent, nombreNode));
+				ouf << "node" << nombreNode << " [label=" << dessi.referent << "];" << endl;
+				nombreNode+=1;
+			}
+
+		}
+
+		//on parcourt la map cette fois pour enregistrer les connexions entre les nodes
+		for(auto itmap = myMapGraph.begin(); itmap != myMapGraph.end(); ++itmap){
+
+			dessi = itmap->second;
+
+			ouf << "node" << myMapNode[dessi.referent] << " -> node" << myMapNode[dessi.cible] << " [label=\"" << dessi.compteur << "\"];" << endl;
+
+		}
+
+		ouf << "}";
+		ouf.close();
+
+	}
+
 }
